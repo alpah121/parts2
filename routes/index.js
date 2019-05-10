5a26dd6ca87acb2739b04dc85841cc94a48bb2f7
jacob <jacob_a_reed@yahoo.com>
2019-05-10 18:24:51 +0000

    added search route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+//router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
@@ -11,5 +11,36 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
     user: req.user
   })
 );
+
+
+var part = require('../models/part');
+
+
+
+router.get('/', function(req, res) {
+	res.render('home');
+	});
+
+router.get('/search', function(req, res) {
+	if (req.query.hasOwnProperty('q'))
+	{
+	part.find({ keywords: req.query.q}, 
+	function (err, parts) 
+		{
+		if (err) console.log(err);
+		res.render('results', {results: parts});
+		});
+	}
+	else
+	{
+	console.log(req.query.q);
+	res.render('results', {results: [
+	{title: 'wheel of pressure washer', goesTo: 'pressure washer', link: 'amazon.com/pressure-washer-wheel'},
+	{title: 'wheel of log cutter', goesTo: 'log cutter', link: 'amazon.com/log-cutter-wheel'},
+	{title: 'sprocket', goesTo: 'dirt bike', link: 'amazon.com/sprocket'},
+	
+	]});
+	}
+});
 
 module.exports = router;
